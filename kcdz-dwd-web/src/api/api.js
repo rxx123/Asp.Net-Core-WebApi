e29390a97cc3b11032c6da2dd9c01a5f5bf9d246
ruxingxing <549589768@qq.com>
2019-05-29 16:11:28 +0800

    ..

--- kcdz-dwd-web/src/api/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-let base = 'http://localhost:54942/api';
-axios.defaults.headers.post['Content-Type'] = 'application/json';
-
-// export const requestLogin = param => { return axios.get(`${base}/Login`, { params: param }); };
-export const requestLogin = param => { return axios.post(`${base}/Login`, param).then(res => res.data); };
-export const requestEquipment = param => { return axios.post(`${base}/EquipmentLedger`, param).then(res => res.data); };
